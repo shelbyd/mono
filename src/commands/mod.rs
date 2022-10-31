e273357a41bad357f88b8443ddfcf8de7dbeb146
Shelby Doolittle <shelby@pinestreetlabs.com>
2022-10-31 01:31:37 -0400

    implement basic sync command to copy files

---
+++ src/commands/mod.rs
@@ -0,0 +1,54 @@
+use std::{collections::HashMap, error::Error, fs::File};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+pub struct SyncCommand {}
+
+impl SyncCommand {
+    pub fn run(self) -> Result<(), Box<dyn Error>> {
+        // TODO(shelbyd): Get root from VCS.
+        let root = ".";
+
+        for result in ignore::Walk::new(root) {
+            let entry = result?;
+            let path = entry.path().strip_prefix("./")?;
+
+            if path.file_name().and_then(|f| f.to_str()) != Some("MONO") {
+                continue;
+            }
+            log::info!("Found MONO file {}", path.display());
+
+            let dir = path.parent().expect("should never be root");
+
+            let monofile: crate::Monofile = serde_yaml::from_reader(File::open(path)?)?;
+
+            let mut interpolation_vars = HashMap::new();
+            let dir_slug = dir.to_string_lossy().replace("/", "-");
+            interpolation_vars.insert("dir_slug".to_string(), dir_slug);
+
+            for sync in &monofile.sync_files {
+                for entry in glob::glob(&dir.join(&sync.from.0).display().to_string())? {
+                    let from = entry?;
+                    interpolation_vars.insert(
+                        "file_name".to_string(),
+                        from.file_name().unwrap().to_string_lossy().to_string(),
+                    );
+
+                    let to = sync.to.interpolate(&interpolation_vars)?;
+                    let to = if to.starts_with("/") {
+                        format!("{}/{}", root, to.trim_start_matches("/"))
+                    } else {
+                        format!("{}/{}", dir.display(), to)
+                    };
+
+                    log::info!("Copying {} to {}", from.display(), &to);
+                    let contents = std::fs::read_to_string(&from)?;
+                    std::fs::write(&to, contents)?;
+                }
+                interpolation_vars.remove("file_name");
+            }
+        }
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use structopt::StructOpt;
+
+mod commands;
+mod monofile;
+
+pub use monofile::*;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let opts = Options::from_args();
+
+    stderrlog::new()
+        .module(module_path!())
+        .verbosity(opts.verbose + 1)
+        .quiet(opts.quiet)
+        .init()?;
+
+    opts.command.run()?;
+    Ok(())
+}
+
+#[derive(StructOpt)]
+struct Options {
+    /// Silence all output.
+    #[structopt(short = "q", long = "quiet")]
+    quiet: bool,
+
+    /// Verbose mode (-v, -vv, -vvv, etc).
+    #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
+    verbose: usize,
+
+    /// Command to run.
+    #[structopt(subcommand)]
+    command: Command,
+}
+
+#[derive(StructOpt)]
+enum Command {
+    Sync(commands::SyncCommand),
+}
+
+impl Command {
+    fn run(self) -> Result<(), Box<dyn Error>> {
+        match self {
+            Command::Sync(cmd) => cmd.run(),
+        }
+    }
 }
