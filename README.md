a78931db6a979889c6e5eda307a6e7215b89a6f1
Shelby Doolittle <shelby@pinestreetlabs.com>
2022-10-31 01:56:24 -0400

    add more docs to the README

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 Command line utilities for managing mono-repos.
 
 Meant to be used by developers and in CI/CD.
+
+### Usage
+
+Any subdirectory can have a `MONO` file that specifies how that directory relates to the rest
+of the project.
+
+#### Sync Files
+
+MONO files can specify files to sync from one location in the project to another.
+
+```yaml
+sync_files:
+  - from: .github/workflows/*
+    to: /.github/workflows/${ dir_slug }-${ file_name }
+```
+
+The above config, when located at, `foo/MONO` will copy a file like
+`foo/.github/workflows/test.yml` to `.github/workflows/foo-test.yml`.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 //! Command line utilities for managing mono-repos.
 //!
 //! Meant to be used by developers and in CI/CD.
+//!
+//! ## Usage
+//!
+//! Any subdirectory can have a `MONO` file that specifies how that directory relates to the rest
+//! of the project.
+//!
+//! ### Sync Files
+//!
+//! MONO files can specify files to sync from one location in the project to another.
+//!
+//! ```yaml
+//! sync_files:
+//!   - from: .github/workflows/*
+//!     to: /.github/workflows/${ dir_slug }-${ file_name }
+//! ```
+//!
+//! The above config, when located at, `foo/MONO` will copy a file like
+//! `foo/.github/workflows/test.yml` to `.github/workflows/foo-test.yml`.
 
 use std::error::Error;
 use structopt::StructOpt;
@@ -26,6 +44,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 #[derive(StructOpt)]
+#[structopt(about = "Manage mono-repos")]
 struct Options {
     /// Silence all output.
     #[structopt(short = "q", long = "quiet")]
